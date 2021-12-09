ef3d62d18f119b684ffb2220a457d9a6e08ca451
Juste Klapatauskaite <juste.klapatauskaite@gmail.com>
2021-12-09 02:07:04 +0200

    Main Screen UI added

--- tvTracker/app/src/main/java/com/example/tvtracker/MainActivity.java
+++ tvTracker/app/src/main/java/com/example/tvtracker/MainActivity.java
@@ -12,14 +12,14 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
     private ImageButton notif;
     private Button login;
-    private Button signUp;
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         login = (Button) findViewById(R.id.button_login);
-        signUp = (Button)  findViewById(R.id. button_signUp);
+        signup = (Button)  findViewById(R.id. button_signUp);
 
         login.setOnClickListener(v -> openLoginActivity());
     }
