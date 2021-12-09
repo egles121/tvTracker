71f8e9c727877b53705fd0ea3341809fe9702d1d
Romans Cernihs <cernihsromans@gmail.com>
2021-12-09 12:29:16 +0200

    set up JDBC connection added Classes for DB data handling methods

---
+++ tvTracker/app/src/main/java/com/example/tvtracker/DB/SqlQuery.java
@@ -0,0 +1,12 @@
+package com.example.tvtracker.DB;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqlQuery {
+
+}
